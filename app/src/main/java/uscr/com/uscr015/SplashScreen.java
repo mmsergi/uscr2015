b6c21ff2f6bb721db572a9b9345a165fb8d90c6c
SSLune <oriol2706@gmail.com>
2015-11-11 18:56:44 +0000

    Añadida splash

---
+++ app/src/main/java/uscr/com/uscr015/SplashScreen.java
@@ -0,0 +1,41 @@
+package uscr.com.uscr015;
+
+/**
+ * Created by oriolferrer on 11/11/15.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
